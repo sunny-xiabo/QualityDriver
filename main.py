f811cd81a144e1626443ffb67d0e96e42b52de51
xiabo <bo.xia@miotech.com>
2023-12-18 18:36:54 +0800

    增加日志以及处理逻辑

--- main.py
+++ main.py
@@ -7,6 +7,9 @@
 import click
 import uvicorn
 from fastapi import FastAPI
+from loguru import logger
+
+from autotest.init.logger_init import init_logger
 from config import config
 from autotest.init.routers import init_router
 from autotest.init.redis_init import init_async_redis_pool
@@ -21,9 +24,11 @@ async def init_app():
     注册中心
     :return:
     """
-    init_router(app)
-    init_middleware(app)
-    await init_async_redis_pool(app)
+    init_router(app)  # 注册路由
+    init_middleware(app)  # 注册中间件
+    await init_async_redis_pool(app)  # 连接redis
+    init_logger()  # 初始化日志
+    logger.info("日志启动成功")
 
 
 @app.on_event("startup")
