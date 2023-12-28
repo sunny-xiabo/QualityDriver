9a1d414e73f32b4fb4a30e0ebb673e839e8cd440
xiabo <bo.xia@miotech.com>
2023-12-28 15:43:12 +0800

    隔离样式逻辑

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useCallback } from 'react';
 import { message, Tabs } from 'antd';
 import { login } from '@/services/login/api';
-import ProForm, { ProFormText } from '@ant-design/pro-form';
+import ProForm, { ProFormCheckbox, ProFormText } from '@ant-design/pro-form';
 import { FormattedMessage } from 'react-intl';
 
 import styles from './index.less';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import bgImage from '@/assets/backgroud-login.jpeg';
 
 const submitButtonTextMap: Record<string, string> = {
   account: '登录',
@@ -26,70 +27,103 @@ export default function IndexPage() {
   }, []);
 
   return (
-    <div className={styles.main}>
-      <ProForm
-        onFinish={onSubmit}
-        isKeyPressSubmit
-        submitter={{
-          render: (_, dom) => dom.pop(),
-          searchConfig: {
-            submitText: submitButtonTextMap[type],
-          },
-          submitButtonProps: {
-            size: 'large',
-            style: {
-              widows: '100%',
-              borderRadius: '32px',
+    <div
+      className={styles.main}
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <div className={styles.form_wrap}>
+        <ProForm
+          onFinish={onSubmit}
+          isKeyPressSubmit
+          submitter={{
+            render: (_, dom) => dom.pop(),
+            searchConfig: {
+              submitText: submitButtonTextMap[type],
             },
-          },
-        }}
-      >
-        <Tabs accessKey={type} onChange={setType}>
-          <Tabs.TabPane key="account" tab="账号密码登录" />
-          <Tabs.TabPane key="register" tab="注册" />
-        </Tabs>
-
-        {type === 'account' && (
-          <>
-            <ProFormText
-              name="username"
-              fieldProps={{
-                size: 'large',
-                style: { borderRadius: '24px' },
-                prefix: <UserOutlined className={styles.prefixIcon} />,
-              }}
-              rules={[
-                {
-                  required: true,
-                  message: <FormattedMessage defaultMessage="请输入用户名!" />,
-                },
-              ]}
-              placeholder="请输入用户名"
-            />
-
-            <ProFormText.Password
-              name="password"
-              fieldProps={{
-                size: 'large',
-                style: { borderRadius: '24px' },
-                prefix: <LockOutlined className={styles.prefixIcon} />,
-              }}
-              rules={[
-                {
-                  required: true,
-                  message: <FormattedMessage defaultMessage="请输入密码!" />,
-                },
-              ]}
-              placeholder="请输入密码"
-            />
-          </>
-        )}
-        <div
-          style={{
-            marginBottom: 24,
+            submitButtonProps: {
+              size: 'large',
+              style: {
+                width: '100%',
+                borderRadius: '32px',
+              },
+            },
           }}
-        ></div>
-      </ProForm>
+        >
+          <Tabs accessKey={type} onChange={setType}>
+            <Tabs.TabPane key="account" tab="账号密码登录" />
+            <Tabs.TabPane key="register" tab="注册" />
+          </Tabs>
+          {/* 登录逻辑 */}
+          {type === 'account' && (
+            <>
+              <ProFormText
+                name="username"
+                fieldProps={{
+                  className: styles.proform_generic_warp,
+                  prefix: <UserOutlined className={styles.prefixIcon} />,
+                }}
+                rules={[
+                  {
+                    required: true,
+                    message: '请输入用户名',
+                  },
+                ]}
+                placeholder="这里输入用户名"
+              />
+
+              <ProFormText.Password
+                name="password"
+                fieldProps={{
+                  className: styles.proform_generic_warp,
+                  prefix: <LockOutlined className={styles.prefixIcon} />,
+                }}
+                rules={[
+                  {
+                    required: true,
+                    message: '请输入密码',
+                  },
+                ]}
+                placeholder="这里输入密码"
+              />
+            </>
+          )}
+          {/* 注册逻辑 */}
+          {type=== 'register' && (
+            <>
+              <ProFormText
+                fieldProps={{
+                  className: styles.proform_generic_warp,
+                  prefix: <UserOutlined className={styles.prefixIcon} />
+                }}
+                name="username"
+                placeholder='这里输入用户名'
+                rules={[
+                  {
+                    required: true,
+                    message: '请输入用户名'
+                  }
+                ]}
+              />
+
+            </>
+          )}
+
+
+          <div className={styles.forget_pwd_wrap}>
+            <a
+              onClick={() => {
+                alert('请联系管理员修改密码');
+              }}
+            >
+              忘记密码？
+            </a>
+          </div>
+        </ProForm>
+      </div>
     </div>
   );
 }
--- frontend/src/typing.d.ts
+++ frontend/src/typing.d.ts
@@ -1,3 +1,8 @@
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+
 interface IResult <T>{
     data:T;
     code: number;
