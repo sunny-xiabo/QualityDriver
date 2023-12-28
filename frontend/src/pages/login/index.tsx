b1b1ba2471358fe150da9435da148924ae90065a
xiabo <bo.xia@miotech.com>
2023-12-28 17:42:01 +0800

    login页去除注册逻辑

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,16 +1,15 @@
 import React, { useState, useCallback } from 'react';
 import { message, Tabs } from 'antd';
 import { login } from '@/services/login/api';
-import ProForm, { ProFormCheckbox, ProFormText } from '@ant-design/pro-form';
-import { FormattedMessage } from 'react-intl';
+import ProForm, { ProFormText } from '@ant-design/pro-form';
+
 
 import styles from './index.less';
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { LockOutlined, MailOutlined, UserOutlined } from '@ant-design/icons';
 import bgImage from '@/assets/backgroud-login.jpeg';
 
 const submitButtonTextMap: Record<string, string> = {
   account: '登录',
-  register: '注册',
 };
 
 export default function IndexPage() {
@@ -55,7 +54,6 @@ export default function IndexPage() {
         >
           <Tabs accessKey={type} onChange={setType}>
             <Tabs.TabPane key="account" tab="账号密码登录" />
-            <Tabs.TabPane key="register" tab="注册" />
           </Tabs>
           {/* 登录逻辑 */}
           {type === 'account' && (
@@ -91,27 +89,6 @@ export default function IndexPage() {
               />
             </>
           )}
-          {/* 注册逻辑 */}
-          {type=== 'register' && (
-            <>
-              <ProFormText
-                fieldProps={{
-                  className: styles.proform_generic_warp,
-                  prefix: <UserOutlined className={styles.prefixIcon} />
-                }}
-                name="username"
-                placeholder='这里输入用户名'
-                rules={[
-                  {
-                    required: true,
-                    message: '请输入用户名'
-                  }
-                ]}
-              />
-
-            </>
-          )}
-
 
           <div className={styles.forget_pwd_wrap}>
             <a
