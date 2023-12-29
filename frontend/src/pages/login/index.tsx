06d42b7ca0b05a770be926633b3230a00d0ed35a
xiabo <bo.xia@miotech.com>
2023-12-29 16:14:05 +0800

    clear

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -89,7 +89,6 @@ export default function IndexPage() {
               />
             </>
           )}
-
           <div className={styles.forget_pwd_wrap}>
             <a
               onClick={() => {
